4184c72af94a6e4ec1b3225bbc63117a7eb97951
Coldrink24 <117348473+Coldrink24@users.noreply.github.com>
2022-12-25 15:22:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Program
+{
+    class console
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter First number");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second number");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double v;
+            for (double i = 2; i <= num2; i++)
+            {
+                if (Check(i) == true)
+                {
+                    v = ((Math.Pow(2, i) - 1) * Math.Pow(2, i - 1));
+
+                    if (v >= num1 && v <= num2)
+                    {
+
+                        Console.WriteLine(v + " is a perfect.");
+                    }
+
+                }
+
+            }
+
+        }
+
+        public static bool Check(double i)
+        {
+            bool prime = true;
+
+            if (i == 0 || i == 1)
+                prime = false;
+
+            for (double k = 2; k <= i / 2; k++)
+            {
+
+
+                if ((i % k) == 0) prime = false;
+            }
+            return prime;
+        }
+
+    }
+
+
+}
